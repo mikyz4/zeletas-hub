a85ac4f34348d4b172d367080c2f35f902d387b0
Miky <infoselettas@gmail.com>
2026-02-01 20:18:06 +0100

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,11 +1,8 @@
-// ==============================
-// ZELETAS HUB - Supabase Client
-// ==============================
-
+// assets/js/supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-export const SUPABASE_URL = "https://tmiblfkgxzibcpluixxl.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_KcGGtPAUTe1hTs85t15u-A_lphYRfNJ";
+export const SUPABASE_URL = "https://sbqpkfdcznulwpxlxiwu.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_9dX-ESpW0YwCaFfDR0aMAg_9PLvO2oo";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
